201014db40e63e88b1a30264e4d0518d307e3a43
Irfan81Bot <125128497+Irfan81Bot@users.noreply.github.com>
2023-02-11 17:00:53 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -11,17 +11,17 @@ class Var(object):
     MULTI_CLIENT = False
     API_ID = 10956858
     API_HASH = "cceefd3382b44d4d85be2d83201102b7"
-    BOT_TOKEN = "5325357874:AAEEgEkYAo23arFBbDg89qONslSSh9zSYAU"
+    BOT_TOKEN = "6079277283:AAHHBm0vIH_hSky4R8AMQnbZaxtjVwYzsN0"
     name = str(getenv('name', 'filetolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = "-1001574159030"
+    BIN_CHANNEL = "-1001738918633"
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1125671241 1809710185").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1125671241").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
-    APP_NAME = None
+    APP_NAME = "Sorry"
     OWNER_USERNAME = "Irfan50786"
     if 'DYNO' in environ:
         ON_HEROKU = True
@@ -35,6 +35,6 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = "mongodb+srv://Irfan:786or786@cluster0.2jjhd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    DATABASE_URL = "mongodb+srv://dsbotzz:786or786@cluster0.wrbdm8z.mongodb.net/?retryWrites=true&w=majority"
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', "DS_Botz"))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
