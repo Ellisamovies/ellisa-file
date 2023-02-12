4b09a8a49c47ea5ea3967911e5955e192b5cecae
irfan36bot <116106429+irfan36bot@users.noreply.github.com>
2023-02-12 13:46:17 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -16,12 +16,12 @@ class Var(object):
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
     BIN_CHANNEL = "-1001738918633"
-    PORT = int(getenv('PORT', 8080))
+    PORT = int(getenv('PORT', 8000))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1125671241").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
-    APP_NAME = "Sorry"
+    APP_NAME = ""
     OWNER_USERNAME = "Irfan50786"
     if 'DYNO' in environ:
         ON_HEROKU = True
